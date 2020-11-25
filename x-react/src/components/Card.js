6e64b0dd574c11e77e3a6912295c1d8552b5a42a
Scott Campbell <stc9486@gmail.com>
2020-11-25 15:29:11 -0500

    robohas

---
+++ x-react/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Card = props => {
+  return (
+    <div>
+      <img src='https://robohash.org/REACT?200X200' alt='alt' />
+      <div>
+        <h2>Name</h2>
+        <p>name@email.com</p>
+      </div>
+    </div>
+  );
+};
+
+// Card.propTypes = {
+  
+// };
+
+export default Card;
